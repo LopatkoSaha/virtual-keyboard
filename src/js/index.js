996804e4b7dd33d24b728d5ea09fc6c0577c8e92
Саха <lopatkoaleksandr2@gmail.com>
2023-04-28 23:17:56 +0300

    feat: add style keyboard

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import '../scss/main.scss';
-// import Keyboard from './Keyboard';
-// import dataEng from './dataEng';
+import Keyboard from './Keyboard';
+import dataEng from './dataEng';
 
 document.body.insertAdjacentHTML('beforeend', `
   <div class="wrapper">
       <div class="legend"></div>
+      <div class="output"></div>
       <div class="keyboard"></div>
-      <textarea class="output"></textarea>
   </div>
 `);
 
-// const keyboard = new Keyboard(dataEng);
+const keyboard = new Keyboard(dataEng);
